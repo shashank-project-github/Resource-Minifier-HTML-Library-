d7dfe3f0ec05dc9a11c64f85a5029c7399152070
shashank-project-github <shashankverma9919310939@gmail.com>
2024-07-16 18:21:08 +0530

    changes

--- frontend/src/app/Navbar.jsx
+++ frontend/src/app/Navbar.jsx
@@ -168,7 +168,7 @@ const Navbar = () => {
                   <li>
                     <Link
                       href="/signup"
-                      className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 block md:hidden"
+                      className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700  md:hidden"
                     >
                       Signup
                     </Link>
@@ -176,7 +176,7 @@ const Navbar = () => {
                   <li>
                     <Link
                       href="/login"
-                      className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 block md:hidden"
+                      className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700  md:hidden"
                     >
                       Login
                     </Link>
